ca672d8e394152136a22a027e7c356437a3af6b6
Fernando <fernandoasfj+github@gmail.com>
2021-02-15 22:35:19 +0100

    Introduces characters

---
+++ database/seeders/CharacterSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Character;
+
+class CharacterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Character::factory()->create([
+            'name' => 'Blanka'
+        ]);
+    }
+}
