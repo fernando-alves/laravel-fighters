63b469c74ebec8db3144458306b4d002f0ddb01a
Fernando <fernandoasfj+github@gmail.com>
2021-02-15 22:38:46 +0100

    Reroute landing page to character list

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -14,7 +14,7 @@ class CharacterController extends Controller
      */
     public function index()
     {
-        //
+        return view('characters.index');
     }
 
     /**
---
+++ resources/views/characters/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Character list</h1>
