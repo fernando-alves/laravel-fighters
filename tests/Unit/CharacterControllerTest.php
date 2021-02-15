9953105fcf5d964619a8eb757ed95eca928e4cdd
Fernando <fernandoasfj+github@gmail.com>
2021-02-16 00:53:29 +0100

    Basic test for character controller

---
+++ tests/Unit/CharacterControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\CharacterController;
+use App\Models\Character;
+
+class CharacterControllerTest extends TestCase
+{
+    public function test_creates_character_from_name()
+    {
+        $request = Request::create('/store', 'POST',['name' => 'ryu']);
+        $controller = new CharacterController();
+
+        $response = $controller->store($request);
+
+        $character = Character::where('name', 'ryu')->first();
+        $this->assertNotNull($character);
+    }
+
+
+    public function test_fails_validation_if_name_is_missing()
+    {
+        $this->expectException(ValidationException::class);
+        $request = Request::create('/store', 'POST',[]);
+        $controller = new CharacterController();
+
+        $response = $controller->store($request);
+    }
+}
